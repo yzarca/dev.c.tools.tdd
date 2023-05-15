f54064d621cb667568fd4861b74fd6d62e370114
yzarca <yzarca@gmail.com>
2023-05-16 00:27:25 +0200

    Clean Up Folder

--- tdd/lib/tddunit.c
+++
@@ -1,8 +0,0 @@
-// File: tddunit.c
-
-#include "tddunit.h"
-
-static TDDUnit* TDDUnit_New( size_t ) ;
-static void TDDUnit_AddAssert( TDDUnit* , FN_ASSERT ) ;
-static void TDDUnit_RunAssertions( TDDUnit* ) ;
-static void TDDUnit_ShowResult( TDDUnit* ) ;
